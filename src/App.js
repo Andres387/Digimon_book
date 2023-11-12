bf9e8131a4d4acffdecad592b5f765248bfa1d19
prybox <ramolina08@misena.edu.co>
2023-11-11 23:15:44 -0500

    con buscador

--- src/App.js
+++ src/App.js
@@ -10,17 +10,76 @@ import ItemDigimonList from './components/ItemDigimonList';
 // }
 function App() {
   const[ListDigimons, setListDigimons] = React.useState([]);
+  const[displayDigimons, setDisplayDigimons] = React.useState([]);
+  const[search, setSearch] = React.useState("");
+  // const[shouldUpdate, setShouldUpdate] = React.useState(false);
+
+  // const myFunction = React.useCallback((parametro1, parametro2) => {
+
+  // }[shouldUpdate]);
+
+  // const myObjeto = React.useMemo ({},[shouldUpdate]);
+
+  // const myObjeto1 = React.useRef (()=> 4 + 5);
+  const myInput = React.useRef (null);
+
+  React.useEffect (()=>{
+    myInput.current.addEventListener('keyup', ()=>{
+                                                    console.log(myInput.current.value);
+                                                    setSearch(myInput.current.value);
+                                                  })
+  },[])
+
+  React.useEffect(()=>{
+    const filteredData = ListDigimons.filter((digimon) => {
+      console.log(digimon.name, search)
+      return digimon.name.toLowerCase().includes(search.toLowerCase()) ||
+      digimon.level.toLowerCase().includes(search.toLowerCase())
+    });
+    setDisplayDigimons(filteredData);
+    },[search]);
+  // React.useEffect(()=>{
+  //   console.log("render function");
+  //     // render monting  did monting
+  //   },[myFunction, myObject]);
 
   React.useEffect(()=>{
     fetch("https://digimon-api.vercel.app/api/digimon")
       .then(response =>response.json())
-      .then(dataJson =>{setListDigimons(dataJson)});
+      .then(dataJson =>{
+        setListDigimons(dataJson);
+        setDisplayDigimons(dataJson);
+      });
   },[])
+
+  React.useEffect(()=>{
+    console.log("disparo render");
+    //updatin render monting
+  });
+  React.useEffect(()=>{
+  console.log("disparo render did monting");
+    // render monting  did monting
+  },[]);
+
+  React.useEffect(()=>{
+    console.log("disparo render should up monting");
+      // render monting  did monting
+      setTimeout(()=> {
+        // setShouldUpdate(true)
+      },2000);
+    },[]);
+
+    React.useEffect(()=>{
+      return ()=> null;
+        // component will  Un monting
+      },[]);
+  
 
   return (
     <div className="App">
       <h1>Digimon PryyBook</h1>
-      {ListDigimons.map(digimon => <ItemDigimonList 
+      <input ref={myInput}/>
+      {displayDigimons.map(digimon => <ItemDigimonList 
           name={digimon.name} 
           img={digimon.img} 
           level={digimon.level}
